41efee43599518e3fb69a064442ee20977aff16f
unknown <bodnarash1999@gmail.com>
2025-03-28 21:40:46 +0100

    Adjusting logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ export default function Header() {
   return (
     <div className="flex flex-row gap-4 justify-between items-center lg:mx-38 md:mx-24 sm:mx-14 mx-4 md:px-8 sm:px-4 px-2 sm:py-4 py-2 border-x border-gray-700">
       <div className="flex flex-row justify-center items-center">
-        <img src="../../public/icon.png" alt="logo" className="md:w-14 sm:w-12 w-8" />
+        <img src="/icon.png" alt="logo" className="md:w-14 sm:w-12 w-8" />
         <h1 className="lg:text-2xl md:text-xl sm:text-lg text-sm font-bold italic text-slate-800">
           building estimator 1.0
         </h1>
